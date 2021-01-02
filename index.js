a7384ea8ac2f0f2fac2ffb084c1107f9aa6a01d1
Debbie Kirchner <dkirchner4267@gmail.com>
2021-01-01 21:52:19 -0600

    hide mongo uri info

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
 app.use(bodyParser.json());
+console.log(process.env.MONGO_URI);
 
 mongoUri = 'mongodb+srv://dkirchner4267:Damk1232@cluster0.xl4pp.mongodb.net/<dbname>?retryWrites=true&w=majority'
-mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true })
 
 mongoose.connection.on('connected', () => {
     console.log('connected to db');
