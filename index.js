8fdd72846a465e3333b0b59e30d53f19119aba46
Debbie Kirchner <dkirchner4267@gmail.com>
2021-01-01 21:53:03 -0600

    protect mongo uri

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 app.use(bodyParser.json());
 console.log(process.env.MONGO_URI);
 
-mongoUri = 'mongodb+srv://dkirchner4267:Damk1232@cluster0.xl4pp.mongodb.net/<dbname>?retryWrites=true&w=majority'
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true })
 
 mongoose.connection.on('connected', () => {
